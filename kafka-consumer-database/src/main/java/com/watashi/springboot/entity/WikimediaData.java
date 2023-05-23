434d41fc5028571a957bfa5dbc6c40ba252289b4
owner <oredev@proton.me>
2023-05-23 19:44:24 +0545

    Postgres handles LOB differently than MySql

--- kafka-consumer-database/src/main/java/com/watashi/springboot/entity/WikimediaData.java
+++ kafka-consumer-database/src/main/java/com/watashi/springboot/entity/WikimediaData.java
@@ -3,6 +3,8 @@ package com.watashi.springboot.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Length;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name="wikimedia_recent_changes")
@@ -14,6 +16,7 @@ public class WikimediaData {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
+
+    @Column(name = "doc_txt", length = Length.LONG32)
     private String wikiEventData;
 }
